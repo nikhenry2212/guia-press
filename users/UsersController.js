f51ca1febacc705c9367c2aa7579aac672f503ff
nikolas <nikolaspg74@gmail.com>
2022-04-06 11:27:33 -0300

    fix: criando rotas para criar usearios e views de create e listagem do msm

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./Users');
+const bcrypt = require('bcryptjs');
 
 
 router.get('/admin/users', (req, res) => {
-    res.send("Listagem de usuários");
+    User.findAll().then((users) => {
+        res.render('admin/users/lista-users', { users: users });
+    })
 });
 router.get('/admin/users/create', (req, res) => {
     res.render('admin/users/create');
+});
+
+router.post('/users/create', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    User.findOne({ where: { email: email } }).then(user => {
+        if (user === undefined) {
+
+            let salt = bcrypt.genSaltSync(10);
+            let hash = bcrypt.hashSync(password, salt);
+
+            User.create({
+                email: email,
+                password: hash
+            }).then(() => {
+                res.redirect('/');
+            }).catch(err => {
+                res.redirect('/');
+
+            });
+        } else {
+            res.redirect('/admin/users/create');
+        }
+    })
+
+
 })
 module.exports = router;
